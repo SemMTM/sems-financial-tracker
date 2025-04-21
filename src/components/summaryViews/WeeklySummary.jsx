7c3bc0d725fa21a8f190859268f9eb7b9db4426f
SemMTM <osama.hendawi@hotmail.com>
2025-04-21 11:55:38 +0100

    FEAT: Add weekly summary info to table and dynamic rows

--- src/components/summaryViews/WeeklySummary.jsx
+++ src/components/summaryViews/WeeklySummary.jsx
@@ -7,7 +7,7 @@ import styles from '../../styles/WeeklySummary.module.css'
 
 export default function WeeklySummary() {
   const { user } = useAuth()
-  const [weeklySummary, setSummary] = useState([])
+  const [weeklySummary, setSummary] = useState({ weeks: [] })
   const { dataVersion } = useFinancialData();
   const [error, setError] = useState('')
 
@@ -46,149 +46,22 @@ export default function WeeklySummary() {
             </tr>
           </thead>
           <tbody>
-            <tr>
-              <td>1</td>
-              <td>+£200</td>
-              <td>-£433</td>
-              <td>+£155</td>
-            </tr>
-            <tr>
-              <td>2</td>
-              <td>+£200</td>
-              <td>-£45</td>
-              <td>+£155</td>
-            </tr>
-            <tr>
-              <td>3</td>
-              <td>+£200</td>
-              <td>-£45</td>
-              <td>+£155</td>
-            </tr>
-            <tr>
-              <td>4</td>
-              <td>+£200</td>
-              <td>-£45</td>
-              <td>+£155</td>
-            </tr>
-            <tr>
-              <td>5</td>
-              <td>+£200</td>
-              <td>-£45</td>
-              <td>+£155</td>
-            </tr>
+          {[0, 1, 2, 3, 4].map((i) => {
+            const week = weeklySummary.weeks[i];
+            return (
+              <tr key={i}>
+                <td>{i + 1}</td>
+                <td className="income-summary">+{week?.income || 'N/A'}</td>
+                <td className="expenditure-summary">-{week?.cost || 'N/A'}</td>
+                <td className={week?.summary?.includes('-') ? 'expenditure-summary' : 'income-summary'
+                  } 
+                >
+                  {week?.summary || 'N/A'}</td>
+              </tr>
+            );
+          })}
           </tbody>
         </table>
-
-
-        {/*
-        <div className={styles['week-table-h-sec']}>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>Expenditure</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>Income</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>Summary</span>
-              </div>
-          </div>
-        </div>
-
-        <div className={styles['week-table-h-sec']}>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>-£2443334  4434ddssdddsddddeww00</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span >+£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>£200</span>
-              </div>
-          </div>
-        </div>
-
-        <div className={styles['week-table-h-sec']}>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>-£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span >+£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>£200</span>
-              </div>
-          </div>
-        </div>
-
-        <div className={styles['week-table-h-sec']}>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>-£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span >+£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>£200</span>
-              </div>
-          </div>
-        </div>
-
-        <div className={styles['week-table-h-sec']}>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>-£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span >+£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>£200</span>
-              </div>
-          </div>
-        </div>
-
-        <div className={styles['week-table-h-sec']}>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>-£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span >+£200</span>
-              </div>
-          </div>
-          <div className={styles['week-table-v-sec']}>
-              <div className="flex">
-                  <span>£200</span>
-              </div>
-          </div>
-        </div>
-        */}
       </div>
       
     </div>
