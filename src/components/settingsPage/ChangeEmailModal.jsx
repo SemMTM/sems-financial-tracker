5889cd41d2fa7aa3adf037b8f3e5ad67062dc46d
SemMTM <osama.hendawi@hotmail.com>
2025-04-28 13:24:07 +0100

    CHORE: add functional change email modal to settings dropdown

--- src/components/settingsPage/ChangeEmailModal.jsx
+++ src/components/settingsPage/ChangeEmailModal.jsx
@@ -3,22 +3,31 @@ import api from '../../api/axiosDefaults';
 import { useAuth } from '../../context/AuthContext'
 
 const ChangeEmailModal = ({ onClose, setSuccess }) => {
-  const { user } = useAuth();
+  const { user, setUser } = useAuth();
   const [email, setEmail] = useState('');
   const [error, setError] = useState('');
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
+
+    if (email === user.email) {
+      setError('New email is the same as the current email.')
+      return
+    }
+
     setIsSubmitting(true);
     setError('');
     
     try {
-      const res = await api.put('/dj-rest-auth/user/', { email });
+      // 1. Send PATCH request to update email
+      const response = await api.put('/change-email', { email: email });
+      setUser(response.data)
       setSuccess('Email successfully changed!');
       onClose();
     } catch (err) {
       setError('Failed to change email. Please try again.');
+      console.error(err)
     } finally {
       setIsSubmitting(false);
     }
@@ -32,7 +41,8 @@ const ChangeEmailModal = ({ onClose, setSuccess }) => {
 
           <div className="form-input-con">
             {user.email ? (
-              <p>Current Email Address: {user.email}</p>
+              <p>Current Email Address:<br></br>
+                <strong>{user.email}</strong></p>
             ) : (
               <p>No email address associated with this account.</p>
             )}
@@ -54,7 +64,7 @@ const ChangeEmailModal = ({ onClose, setSuccess }) => {
           <button type="submit" disabled={isSubmitting}>
             {isSubmitting ? 'Submitting...' : 'Submit'}
           </button>
-          
+
         </form>
       </div>
     </div>
